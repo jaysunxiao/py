d3bf910d3cbccbe14205d476548a2af55f4f11ca
sun <jaysunxiao@gmail.com>
2023-08-25 17:10:06 +0800

    update

--- ProtocolTest.py
+++ ProtocolTest.py
@@ -1,13 +1,10 @@
 # encoding: UTF-8
 
 from unittest import TestCase
-
+import struct
 
 class ByteBufferTestCase(TestCase):
     def test_bytearray(self):
-        a = bytearray(b'aa')
-        b = a[:]
-        b[1] = ord('b')
-
-        self.assertEqual(a, b'aa')
-        self.assertEqual(b, b'ab')
+        buffer = bytearray(10)
+        for byte in buffer:
+            print(byte)  # 以字节形式输出每个字节的值
--- protocol/buffer/ByteBuffer.py
+++ protocol/buffer/ByteBuffer.py
@@ -1,7 +1,21 @@
 import struct
 
-class ByteBuffer(object):
 
+class ByteBuffer(object):
     writeOffset = 0
     readOffset = 0
     buffer = bytearray(64)
+
+    def setWriteOffset(self, writeOffset):
+        if self.writeOffset > len(self.buffer):
+            raise ValueError("index out of bounds exception:readerIndex:" + str(self.readOffset) +
+                             ", writerIndex:" + str(self.writeOffset) +
+                             "(expected:0 <= readerIndex <= writerIndex <= capacity:" + str(len(self.buffer)))
+        self.writeOffset = writeOffset
+
+    def setReadOffset(self, readOffset):
+        if readOffset > self.writeOffset:
+            raise ValueError("index out of bounds exception:readerIndex:" + str(self.readOffset) +
+                             ", writerIndex:" + str(self.writeOffset) +
+                             "(expected:0 <= readerIndex <= writerIndex <= capacity:" + str(len(self.buffer)))
+        self.readOffset = readOffset
