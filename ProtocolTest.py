a21fbcd9df1a1d658886f611b5f3fcfc46b7c006
godotg <jaysunxiao@gmail.com>
2023-08-26 01:13:02 +0800

    update

--- ProtocolTest.py
+++ ProtocolTest.py
@@ -6,8 +6,9 @@ import struct
 import protocol.buffer.byte_buffer as byte_buffer
 
 
-def print_bytearray(bytes):
-    for byte in bytes:
+def print_bytearray(array):
+    signed_byte_array = struct.unpack('b' * len(array), array)
+    for byte in signed_byte_array:
         print(byte)  # 以字节形式输出每个字节的值
 
 
@@ -16,16 +17,46 @@ class ByteBufferTestCase(TestCase):
         buffer = bytearray(10)
         for byte in buffer:
             print(byte)  # 以字节形式输出每个字节的值
+        pass
+
+    def test_buffer_int(self):
+        byteBuffer = byte_buffer.ByteBuffer()
+        byteBuffer.writeInt(2147483647)
+        self.assertEqual(byteBuffer.readInt(), 2147483647)
+        byteBuffer.writeInt(-2147483648)
+        self.assertEqual(byteBuffer.readInt(), -2147483648)
+        pass
+
+    def test_buffer_long(self):
+        byteBuffer = byte_buffer.ByteBuffer()
+        byteBuffer.writeLong(9223372036854775807)
+        self.assertEqual(byteBuffer.readLong(), 9223372036854775807)
+        print_bytearray(byteBuffer.buffer)
+        byteBuffer.writeLong(-9223372036854775808)
+        self.assertEqual(byteBuffer.readLong(), -9223372036854775808)
+        pass
 
     def test_buffer(self):
         byteBuffer = byte_buffer.ByteBuffer()
         print(byteBuffer.writeOffset)
         print(byteBuffer.readOffset)
         print(byteBuffer.getCapacity())
+        byteBuffer.writeBool(True)
+        self.assertEqual(byteBuffer.readBool(), True)
         byteBuffer.writeByte(-100)
         self.assertEqual(byteBuffer.readByte(), -100)
-        byteBuffer.writeShort(256)
-        self.assertEqual(byteBuffer.readShort(), 256)
+        byteBuffer.writeShort(100)
 
+        self.assertEqual(byteBuffer.readShort(), 100)
+        byteBuffer.writeInt(9999)
+        self.assertEqual(byteBuffer.readInt(), 9999)
+        byteBuffer.writeInt(-9999)
+        self.assertEqual(byteBuffer.readInt(), -9999)
+
+
+        byteBuffer.writeLong(9999)
+        self.assertEqual(byteBuffer.readLong(), 9999)
+        byteBuffer.writeLong(-9999)
+        self.assertEqual(byteBuffer.readLong(), -9999)
         print("----------------------------------------------------------------")
         print_bytearray(byteBuffer.buffer)
--- protocol/buffer/byte_buffer.py
+++ protocol/buffer/byte_buffer.py
@@ -1,7 +1,11 @@
 import struct
 
-
 maxSize = 655537
+maxInt = 2147483647
+minInt = -2147483648
+maxLong = 9223372036854775807
+minLong = -9223372036854775808
+
 
 class ByteBuffer():
     writeOffset = 0
@@ -25,7 +29,6 @@ class ByteBuffer():
     def getCapacity(self):
         return len(self.buffer) - self.writeOffset
 
-
     def ensureCapacity(self, minCapacity):
         while (minCapacity - self.getCapacity() > 0):
             newSize = len(self.buffer) * 2
@@ -34,6 +37,18 @@ class ByteBuffer():
             # auto grow capacity
             self.buffer.extend(bytearray(len(self.buffer)))
 
+    def writeBool(self, value):
+        if value:
+            self.writeByte(1)
+        else:
+            self.writeByte(0)
+
+    def readBool(self):
+        if self.readByte():
+            return True
+        else:
+            return False
+
     def writeByte(self, value):
         self.ensureCapacity(1)
         struct.pack_into('>b', self.buffer, self.writeOffset, value)
@@ -41,6 +56,16 @@ class ByteBuffer():
 
     def readByte(self):
         value = struct.unpack_from('>b', self.buffer, self.readOffset)[0]
+        self.readOffset = self.readOffset + 1
+        return value
+
+    def writeUByte(self, value):
+        self.ensureCapacity(1)
+        struct.pack_into('>B', self.buffer, self.writeOffset, value)
+        self.writeOffset = self.writeOffset + 1
+
+    def readUByte(self):
+        value = struct.unpack_from('>B', self.buffer, self.readOffset)[0]
         self.readOffset = self.readOffset + 1
         return value
 
@@ -53,3 +78,154 @@ class ByteBuffer():
         value = struct.unpack_from('>h', self.buffer, self.readOffset)[0]
         self.readOffset = self.readOffset + 2
         return value
+
+    def writeInt(self, value):
+        if not (minInt <= value <= maxInt):
+            raise ValueError('value must range between minInt:-2147483648 and maxInt:2147483647')
+        value = (value << 1) ^ (value >> 31)
+        self.ensureCapacity(5)
+        if value >> 7 == 0:
+            self.writeUByte(value)
+            return
+
+        if value >> 14 == 0:
+            self.writeUByte(value & 0x7F | 0x80)
+            self.writeUByte((value >> 7) & 0x7F)
+            return
+
+        if value >> 21 == 0:
+            self.writeUByte((value & 0x7F) | 0x80)
+            self.writeUByte(((value >> 7) & 0x7F | 0x80))
+            self.writeUByte((value >> 14) & 0x7F)
+            return
+
+        if value >> 28 == 0:
+            self.writeUByte(value & 0x7F | 0x80)
+            self.writeUByte(((value >> 7) & 0x7F | 0x80))
+            self.writeUByte(((value >> 14) & 0x7F | 0x80))
+            self.writeUByte((value >> 21) & 0x7F)
+            return
+
+        self.writeUByte(value & 0x7F | 0x80)
+        self.writeUByte(((value >> 7) & 0x7F | 0x80))
+        self.writeUByte(((value >> 14) & 0x7F | 0x80))
+        self.writeUByte(((value >> 21) & 0x7F | 0x80))
+        self.writeUByte((value >> 28) & 0x7F)
+        pass
+
+    def readInt(self):
+        b = self.readUByte()
+        value = b & 0x7F
+        if (b & 0x80) != 0:
+            b = self.readUByte()
+            value |= (b & 0x7F) << 7
+            if (b & 0x80) != 0:
+                b = self.readUByte()
+                value |= (b & 0x7F) << 14
+                if (b & 0x80) != 0:
+                    b = self.readUByte()
+                    value |= (b & 0x7F) << 21
+                    if (b & 0x80) != 0:
+                        b = self.readUByte()
+                        value |= (b & 0x7F) << 28
+        return (value >> 1) ^ -(value & 1)
+
+    def writeLong(self, value):
+        if not (minLong <= value <= maxLong):
+            raise ValueError('value must range between minLong:-9223372036854775808 and maxLong:9223372036854775807')
+        value = (value << 1) ^ (value >> 63)
+        self.ensureCapacity(9)
+        if value >> 7 == 0:
+            self.writeUByte(value)
+            return
+
+        if value >> 14 == 0:
+            self.writeUByte(value & 0x7F | 0x80)
+            self.writeUByte((value >> 7) & 0x7F)
+            return
+
+        if value >> 21 == 0:
+            self.writeUByte((value & 0x7F) | 0x80)
+            self.writeUByte(((value >> 7) & 0x7F | 0x80))
+            self.writeUByte((value >> 14) & 0x7F)
+            return
+
+        if value >> 28 == 0:
+            self.writeUByte(value & 0x7F | 0x80)
+            self.writeUByte(((value >> 7) & 0x7F | 0x80))
+            self.writeUByte(((value >> 14) & 0x7F | 0x80))
+            self.writeUByte((value >> 21) & 0x7F)
+            return
+        if value >> 35 == 0:
+            self.writeUByte(value & 0x7F | 0x80)
+            self.writeUByte(((value >> 7) & 0x7F | 0x80))
+            self.writeUByte(((value >> 14) & 0x7F | 0x80))
+            self.writeUByte(((value >> 21) & 0x7F | 0x80))
+            self.writeUByte((value >> 28) & 0x7F)
+            return
+        if value >> 42 == 0:
+            self.writeUByte(value & 0x7F | 0x80)
+            self.writeUByte(((value >> 7) & 0x7F | 0x80))
+            self.writeUByte(((value >> 14) & 0x7F | 0x80))
+            self.writeUByte(((value >> 21) & 0x7F | 0x80))
+            self.writeUByte(((value >> 28) & 0x7F | 0x80))
+            self.writeUByte((value >> 35) & 0x7F)
+            return
+        if value >> 49 == 0:
+            self.writeUByte(value & 0x7F | 0x80)
+            self.writeUByte(((value >> 7) & 0x7F | 0x80))
+            self.writeUByte(((value >> 14) & 0x7F | 0x80))
+            self.writeUByte(((value >> 21) & 0x7F | 0x80))
+            self.writeUByte(((value >> 28) & 0x7F | 0x80))
+            self.writeUByte(((value >> 35) & 0x7F | 0x80))
+            self.writeUByte((value >> 42) & 0x7F)
+            return
+        if value >> 56 == 0:
+            self.writeUByte(value & 0x7F | 0x80)
+            self.writeUByte(((value >> 7) & 0x7F | 0x80))
+            self.writeUByte(((value >> 14) & 0x7F | 0x80))
+            self.writeUByte(((value >> 21) & 0x7F | 0x80))
+            self.writeUByte(((value >> 28) & 0x7F | 0x80))
+            self.writeUByte(((value >> 35) & 0x7F | 0x80))
+            self.writeUByte(((value >> 42) & 0x7F | 0x80))
+            self.writeUByte((value >> 49) & 0x7F)
+            return
+        self.writeUByte(value & 0x7F | 0x80)
+        self.writeUByte(((value >> 7) & 0x7F | 0x80))
+        self.writeUByte(((value >> 14) & 0x7F | 0x80))
+        self.writeUByte(((value >> 21) & 0x7F | 0x80))
+        self.writeUByte(((value >> 28) & 0x7F | 0x80))
+        self.writeUByte(((value >> 35) & 0x7F | 0x80))
+        self.writeUByte(((value >> 42) & 0x7F | 0x80))
+        self.writeUByte(((value >> 49) & 0x7F | 0x80))
+        self.writeUByte(value >> 56)
+        pass
+
+    def readLong(self):
+        b = self.readUByte()
+        value = b & 0x7F
+        if (b & 0x80) != 0:
+            b = self.readUByte()
+            value |= (b & 0x7F) << 7
+            if (b & 0x80) != 0:
+                b = self.readUByte()
+                value |= (b & 0x7F) << 14
+                if (b & 0x80) != 0:
+                    b = self.readUByte()
+                    value |= (b & 0x7F) << 21
+                    if (b & 0x80) != 0:
+                        b = self.readUByte()
+                        value |= (b & 0x7F) << 28
+                        if (b & 0x80) != 0:
+                            b = self.readUByte()
+                            value |= (b & 0x7F) << 35
+                            if (b & 0x80) != 0:
+                                b = self.readUByte()
+                                value |= (b & 0x7F) << 42
+                                if (b & 0x80) != 0:
+                                    b = self.readUByte()
+                                    value |= (b & 0x7F) << 49
+                                    if (b & 0x80) != 0:
+                                        b = self.readUByte()
+                                        value |= b << 56
+        return (value >> 1) ^ -(value & 1)
