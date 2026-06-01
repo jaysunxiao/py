7ced78a5ed95eeefe0dc4c1a622324a475c7d267
jaysunxiao <jaysunxiao@gmail.com>
2026-06-01 18:19:34 +0800

    update

--- base/annotation.py
+++ base/annotation.py
@@ -1,3 +1,6 @@
+import time
+
+
 def my_decorator(func):
     def wrapper():
         print("Something is happening before the function is called.")
@@ -10,3 +13,22 @@ def say_hello():
     print("Hello!")
 
 say_hello()
+
+
+print("---------------------------------------------------------------------------------------------------------------------- \n")
+
+def decorator(func):
+    def punch(*args, **kwargs):
+        print(time.strftime('%Y-%m-%d', time.localtime(time.time())))
+        func(*args, **kwargs)
+
+    return punch
+
+
+@decorator
+def punch(name, department):
+    print(f'昵称：{name}  部门：{department} 上班打卡成功')
+
+
+
+punch('两点水', '做鸭事业部')
