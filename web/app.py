76c58c25a2e71b888d9835c302c5ce9476bca950
jaysunxiao <jaysunxiao@gmail.com>
2026-06-02 23:14:52 +0800

    update

--- web/app.py
+++ web/app.py
@@ -1,12 +1,14 @@
-# save this as app.py
-from flask import Flask
+from fastapi import FastAPI
+import asyncio
+import uvicorn
+app = FastAPI()
 
-app = Flask("my web server")
+@app.get("/async")
+async def async_api():
+    await asyncio.sleep(5)
+    return {"msg": "done"}
 
 
-@app.route("/")
-def hello():
-    return "Hello, World!"
 
-
-app.run()
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
